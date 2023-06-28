bc34f8cadbbcf058a67d9168942626010a55dd1d
Talitaventura16 <Talitaventura16@outlook.com>
2023-06-28 13:59:53 -0300

    Escrevi meu primeiro hello_world em fortran e o compilei.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Fortran
+Este é o meu primeiro programa em Fortran!
+
+Seja bem-vindo ao mundo do Fortran ✨ 
---
+++ hello_world.f90
@@ -0,0 +1,11 @@
+program hello_world
+    ! Este é um programa em Fortran que imprime "Hello, World!"
+
+    implicit none
+    ! Declaração de variáveis
+
+    ! Bloco de código principal
+    print *, "Hello, World!"
+
+    ! Fim do programa
+end program hello_world
